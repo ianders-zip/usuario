1c09e2aac410a577a3d7a94983ad5851de2f46bd
Iago Anderson <iagoliveira004@gmail.com>
2026-01-19 00:10:57 -0300

    adicionado métodos get, delete e login

---
+++ src/main/java/com/ianders/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.ianders.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String mensagem){
+        super(mensagem);
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable throwable){
+        super(mensagem,throwable);
+    }
+}
